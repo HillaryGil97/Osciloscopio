cc17026ba14b949ba439c7a0eb449b4aef34a3a1
Hillary Gil <hillarygil97@gmail.com>
2020-11-22 14:16:55 -0600

    dibujo de una senoide

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 //display
 const canva = document.getElementById('draw')
 var ctx = canva.getContext('2d')
+const X = 450
+const Y = 410
 
 //sección de control
 const power = document.getElementsByName('power')
@@ -19,16 +21,14 @@ const time = document.getElementsByName('time')
 var isClick=false;
 
 function gridBegin(){ //Función para dibujar las lineas de escala del osciloscopio
-    let x=450
-    let y=410
-    for(let i=0;i<x;i=i+45){
+    for(let i=0;i<X;i=i+45){
         ctx.beginPath();
         ctx.moveTo(i,0);
         ctx.lineTo(i,410);
         i===225 ? ctx.strokeStyle = 'rgb(13,60,60)': ctx.strokeStyle = 'rgb(35,160,160)';
         ctx.stroke();
     }
-    for(let i=0;i<y;i=i+41){
+    for(let i=0;i<Y;i=i+41){
         ctx.beginPath();
         ctx.moveTo(0,i);
         ctx.lineTo(450,i);
@@ -37,15 +37,22 @@ function gridBegin(){ //Función para dibujar las lineas de escala del oscilosco
     }
 }
 
-function drawSin(){
-    
+function drawSin(frec, amp,des){
+    for(let i=0;i<X;i=i+0.05){
+        let ang = (Math.PI*i)/2
+        let funY = amp*Math.sin((ang*frec) + des)+205
+        ctx.beginPath();
+        ctx.arc(i,funY, 1, 0, Math.PI * 2, true)
+        ctx.strokeStyle = 'rgb(255,0,0)'
+        ctx.stroke();
+    }
 }
 
 const startMe = () => {
     isClick = !isClick
     if(isClick){
         gridBegin()
-
+        drawSin(0.1,100,0)
     }else{
         canva.width=canva.width;
     }
